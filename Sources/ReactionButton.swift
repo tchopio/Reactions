e8ce9e452d15bc692dc5cf5c396c948a2c727dd8
ArtemiyTchop <112002278+ArtemiyTchop@users.noreply.github.com>
2024-04-28 21:51:14 +0300

    Apply new access level for reaction button, for dark mode settings

--- Sources/ReactionButton.swift
+++ Sources/ReactionButton.swift
@@ -35,7 +35,7 @@ import UIKit
  */
 public final class ReactionButton: UIReactionControl {
   private let iconImageView: UIImageView = Components.reactionButton.facebookLikeIcon()
-  private let titleLabel: UILabel        = Components.reactionButton.facebookLikeLabel()
+  let titleLabel: UILabel        = Components.reactionButton.facebookLikeLabel()
   private lazy var overlay: UIView       = UIView().build {
     $0.clipsToBounds   = false
     $0.backgroundColor = .clear
